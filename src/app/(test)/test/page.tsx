cc2a34d1c804e3e4b3a637c55b861352f9969fe1
Jure Rotar <jrotar0@gmail.com>
2023-05-21 23:06:34 +0200

    refactor: moved test page

--- src/app/(test)/test/page.tsx
+++ src/app/(test)/test/page.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useLocation } from 'react-router-dom';
 
-export const TestView: React.FC = () => {
+export const TestPage: React.FC = () => {
   const location = useLocation();
   return (
     <>
