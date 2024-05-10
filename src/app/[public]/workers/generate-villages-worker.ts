29427d0ba10a9d446569117c13b4343d2d30dff5
Jure Rotar <jrotar0@gmail.com>
2024-05-10 11:07:20 +0200

    fix: fixed incorrect db seeding

--- src/app/[public]/workers/generate-villages-worker.ts
+++ src/app/[public]/workers/generate-villages-worker.ts
@@ -35,12 +35,13 @@ self.addEventListener('message', async (event: MessageEvent<GenerateVillageWorke
   self.postMessage({ villages });
 
   const promises = [];
-  const chunkedVillages = chunk(tiles, 300);
+  const chunkedVillages = chunk(villages, 300);
 
   for (let i = 0; i < chunkedVillages.length; i += 1) {
-    promises.push(database.maps.bulkAdd(chunkedVillages[i]));
+    promises.push(database.villages.bulkAdd(chunkedVillages[i]));
   }
 
   await Promise.all(promises);
+
   self.close();
 });
