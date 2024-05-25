eea164146817c85d19280d7cca7570356cb753f2
Jure Rotar <jrotar0@gmail.com>
2024-05-25 12:22:13 +0200

    test: added happy-dom to resolve node 21 deprecation warning,

--- vite.config.ts
+++ vite.config.ts
@@ -61,7 +61,7 @@ export default defineConfig({
   test: {
     watch: false,
     globals: true,
-    environment: 'jsdom',
+    environment: 'happy-dom',
     setupFiles: './vitest-setup.ts',
   },
 }) satisfies UserConfig;
