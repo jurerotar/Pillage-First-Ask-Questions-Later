1ef14e651c89b660f04c22fcb293690d5e2df100
Jure Rotar <jrotar0@gmail.com>
2024-10-15 14:41:04 +0200

    chore: format

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,7 @@ import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({ registerType: 'autoUpdate' }),
-  ],
+  plugins: [react(), VitePWA({ registerType: 'autoUpdate' })],
   server: {
     open: true,
   },
