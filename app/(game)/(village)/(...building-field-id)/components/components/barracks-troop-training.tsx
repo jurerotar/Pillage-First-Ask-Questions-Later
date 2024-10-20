7b4452bf01db123593b6af014230a98e5ed774b4
Jure Rotar <jrotar0@gmail.com>
2024-10-20 22:54:16 +0200

    chore: lint fixes

--- app/(game)/(village)/(...building-field-id)/components/components/barracks-troop-training.tsx
+++ app/(game)/(village)/(...building-field-id)/components/components/barracks-troop-training.tsx
@@ -1,14 +1,8 @@
 import { UnitCard } from 'app/(game)/(village)/(...building-field-id)/components/components/components/unit-card';
-import { useCreateEvent } from 'app/(game)/hooks/use-create-event';
 import { useUnitResearch } from 'app/(game)/hooks/use-unit-research';
-import { GameEventType } from 'app/interfaces/models/events/game-event';
 
 export const BarracksTroopTraining = () => {
   const { researchedUnits } = useUnitResearch();
-
-  console.log(researchedUnits);
-
-  const { createBulkEvent: createBulkBarracksTrainingEvent } = useCreateEvent(GameEventType.TROOP_TRAINING);
 
   return (
     <article>
