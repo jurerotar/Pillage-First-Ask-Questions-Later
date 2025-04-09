f52edbed004ab01a5b7dc6360e8273ba752d722d
Jure Rotar <jrotar0@gmail.com>
2025-04-09 19:44:06 +0200

    perf: improved map mount performance by removing useLayoutEffect

--- app/(game)/(village-slug)/(map)/page.tsx
+++ app/(game)/(village-slug)/(map)/page.tsx
@@ -13,7 +13,7 @@ import { useDialog } from 'app/hooks/use-dialog';
 import type { Point } from 'app/interfaces/models/common';
 import type { Tile as TileType } from 'app/interfaces/models/game/tile';
 import type { Village } from 'app/interfaces/models/game/village';
-import { use, useEffect, useLayoutEffect, useMemo, useRef, useState } from 'react';
+import { use, useMemo, useRef } from 'react';
 import { useSearchParams } from 'react-router';
 import { FixedSizeGrid, FixedSizeList } from 'react-window';
 import { useEventListener } from 'usehooks-ts';
@@ -23,13 +23,15 @@ import type { WorldItem } from 'app/interfaces/models/game/world-item';
 import { isStandaloneDisplayMode } from 'app/utils/device';
 import pageStyles from './page.module.css';
 import { clsx } from 'clsx';
+import { Dialog } from 'app/components/ui/dialog';
+import { TileDialog } from 'app/(game)/(village-slug)/(map)/components/tile-modal';
 
 // Height/width of ruler on the left-bottom.
 const RULER_SIZE = 20;
 
 const MapPage = () => {
   const [searchParams] = useSearchParams();
-  const { isOpen: isTileModalOpened, openModal } = useDialog<TileType>();
+  const { isOpen: isTileModalOpened, openModal, toggleModal, modalArgs } = useDialog<TileType | null>();
   const { map, getTileByTileId } = useMap();
   const { height, width, isWiderThanLg } = use(ViewportContext);
   const { mapFilters } = useMapFilters();
@@ -38,8 +40,6 @@ const MapPage = () => {
   const { reputationsMap } = useReputations();
   const { villages } = useVillages();
   const { worldItems } = useWorldItems();
-
-  const [rerenderCount, setRerenderCount] = useState<number>(0);
 
   const startingX = Number.parseInt(searchParams.get('x') ?? '0');
   const startingY = Number.parseInt(searchParams.get('y') ?? '0');
@@ -63,7 +63,6 @@ const MapPage = () => {
    */
   const mapHeight = isWiderThanLg ? height - 76 : height - 210 - (isPwa ? 48 : 0);
 
-  const previousTileSize = useRef<number>(tileSize);
   const isScrolling = useRef<boolean>(false);
   const currentCenterTile = useRef<Point>({
     x: startingX,
@@ -164,64 +163,26 @@ const MapPage = () => {
     window,
   );
 
-  // biome-ignore lint/correctness/useExhaustiveDependencies: We actually need tileSize, we just use it in currentCenterTile calculation
-  useLayoutEffect(() => {
-    if (!mapRef.current || !bottomMapRulerRef.current || !leftMapRulerRef.current) {
-      return;
-    }
+  const scrollLeft = (centerXTile: number) => {
+    return tileSize * centerXTile + (tileSize * gridSize) / 2 - width / 2;
+  };
 
-    const scrollLeft = (centerXTile: number) => {
-      return tileSize * centerXTile + (tileSize * gridSize) / 2 - width / 2;
-    };
+  const scrollTop = (centerYTile: number) => {
+    return (tileSize * gridSize) / 2 - tileSize * centerYTile - mapHeight / 2;
+  };
 
-    const scrollTop = (centerYTile: number) => {
-      return (tileSize * gridSize) / 2 - tileSize * centerYTile - mapHeight / 2;
-    };
-
-    if (previousTileSize.current !== tileSize) {
-      const offsetX = scrollLeft(currentCenterTile.current.x);
-      const offsetY = scrollTop(currentCenterTile.current.y);
-
-      mapRef.current.scrollTo({
-        top: offsetY,
-        left: offsetX,
-      });
-
-      bottomMapRulerRef.current.scrollTo(offsetX);
-      leftMapRulerRef.current.scrollTo(offsetY);
-
-      previousTileSize.current = tileSize;
-      return;
-    }
-
-    const offsetX = scrollLeft(startingX);
-    const offsetY = scrollTop(startingY);
-
-    mapRef.current.scrollTo({
-      top: offsetY,
-      left: offsetX,
-    });
-
-    bottomMapRulerRef.current.scrollTo(offsetX);
-    leftMapRulerRef.current.scrollTo(offsetY);
-  }, [tileSize]);
-
-  // We need this due to this bug: https://github.com/ReactTooltip/react-tooltip/issues/1189
-  useEffect(() => {
-    // This can be disabled on mobile, because mobile does not render tooltips
-    if (!isWiderThanLg) {
-      return;
-    }
-    if (rerenderCount >= 3) {
-      return;
-    }
-    setRerenderCount((prevState) => prevState + 1);
-  }, [rerenderCount, isWiderThanLg]);
+  const offsetX = scrollLeft(currentCenterTile.current.x);
+  const offsetY = scrollTop(currentCenterTile.current.y);
 
   return (
     <main className="relative overflow-x-hidden overflow-y-hidden scrollbar-hidden">
+      <Dialog
+        open={isTileModalOpened}
+        onOpenChange={toggleModal}
+      >
+        <TileDialog tile={modalArgs.current} />
+      </Dialog>
       <Tooltip
-        key={rerenderCount}
         anchorSelect="[data-tile-id]"
         closeEvents={{
           mouseleave: true,
@@ -240,6 +201,7 @@ const MapPage = () => {
         }}
       />
       <FixedSizeGrid
+        key={tileSize}
         className={clsx('scrollbar-hidden bg-[#8EBF64] will-change-scroll', pageStyles.grid, `magnification--${magnification}`)}
         outerRef={mapRef}
         columnCount={gridSize}
@@ -249,10 +211,8 @@ const MapPage = () => {
         height={mapHeight - 1}
         width={width}
         itemData={fixedGridData}
-        itemKey={({ columnIndex, rowIndex }) => {
-          const tile: TileType = map[gridSize * rowIndex + columnIndex];
-          return tile.id;
-        }}
+        initialScrollLeft={offsetX}
+        initialScrollTop={offsetY}
         onScroll={({ scrollTop, scrollLeft }) => {
           if (bottomMapRulerRef.current) {
             bottomMapRulerRef.current.scrollTo(scrollLeft);
@@ -278,6 +238,7 @@ const MapPage = () => {
           height={mapHeight}
           itemCount={gridSize}
           width={RULER_SIZE}
+          initialScrollOffset={offsetY}
           layout="vertical"
           itemData={{
             layout: 'vertical',
@@ -295,6 +256,7 @@ const MapPage = () => {
           height={RULER_SIZE}
           itemCount={gridSize}
           width={width - RULER_SIZE}
+          initialScrollOffset={offsetX}
           layout="horizontal"
           itemData={{
             layout: 'horizontal',
