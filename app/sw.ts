737d337adfb5285b6f0ce5d1f12eda6a9c2fc5fb
Jure Rotar <jrotar0@gmail.com>
2025-05-10 19:32:11 +0200

    fix: fixed sw html cache

--- app/sw.ts
+++ app/sw.ts
@@ -1,9 +1,10 @@
-import { precacheAndRoute } from 'workbox-precaching';
+import { precacheAndRoute, cleanupOutdatedCaches } from 'workbox-precaching';
 import { clientsClaim } from 'workbox-core';
 
 declare let self: ServiceWorkerGlobalScope;
 
 clientsClaim();
+cleanupOutdatedCaches();
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
