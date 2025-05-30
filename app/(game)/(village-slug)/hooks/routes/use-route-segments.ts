80dfdfb36b4044ccb28941e17cf496a8a8afd468
Jure Rotar <jrotar0@gmail.com>
2025-05-30 16:18:25 +0200

    chore: removed unused testing files

--- app/(game)/(village-slug)/hooks/routes/use-route-segments.ts
+++ app/(game)/(village-slug)/hooks/routes/use-route-segments.ts
@@ -1,17 +1,13 @@
 import type { BuildingField } from 'app/interfaces/models/game/village';
 import { useParams } from 'react-router';
-import { useMemo } from 'react';
 
 export const useRouteSegments = () => {
-  const { serverSlug, villageSlug, buildingFieldId, reportId, questId } = useParams();
+  const { serverSlug, villageSlug, buildingFieldId, reportId } = useParams();
 
-  return useMemo(() => {
-    return {
-      serverSlug: serverSlug as string,
-      villageSlug: villageSlug as string,
-      buildingFieldId: buildingFieldId ? (Number.parseInt(buildingFieldId) as BuildingField['id']) : null,
-      reportId,
-      questId,
-    };
-  }, [serverSlug, villageSlug, buildingFieldId, reportId, questId]);
+  return {
+    serverSlug: serverSlug as string,
+    villageSlug: villageSlug as string,
+    buildingFieldId: buildingFieldId ? (Number.parseInt(buildingFieldId) as BuildingField['id']) : null,
+    reportId,
+  };
 };
