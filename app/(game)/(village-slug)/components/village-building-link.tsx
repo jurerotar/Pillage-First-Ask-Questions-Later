8e58682b25f5e597b4bb3f16e018d60c055f7f5d
Jure Rotar <jrotar0@gmail.com>
2025-07-25 11:41:03 +0200

    perf: added react-compiler

--- app/(game)/(village-slug)/components/village-building-link.tsx
+++ app/(game)/(village-slug)/components/village-building-link.tsx
@@ -7,11 +7,6 @@ import { Link } from 'react-router';
 import { useTranslation } from 'react-i18next';
 import { Text } from 'app/components/text';
 
-const availableBuildingFieldIdSlots = [...new Array(40).keys()].map(
-  (num) => num + 1,
-);
-const availableBuildingFieldIdSlotsSet = new Set(availableBuildingFieldIdSlots);
-
 type VillageBuildingLinkProps = {
   buildingId: Building['id'];
 };
@@ -46,39 +41,14 @@ export const VillageBuildingLink: React.FC<VillageBuildingLinkProps> = ({
     );
   }
 
-  const occupiedBuildingFieldIds = buildingFields.map(({ id }) => id);
-  const occupiedBuildingFieldIdsSet = new Set(occupiedBuildingFieldIds);
-  const emptySlots = availableBuildingFieldIdSlotsSet.difference(
-    occupiedBuildingFieldIdsSet,
-  );
-  const firstEmptySlotId: number | undefined = Math.min(...emptySlots.values());
-
-  if (firstEmptySlotId) {
-    return (
-      <Text
-        as="span"
-        data-tooltip-id="general-tooltip"
-        data-tooltip-content={t(
-          '{{buildingName}} does not exist in this village, first empty building slot will be opened instead',
-          { buildingName },
-        )}
-        variant="link"
-      >
-        <Link
-          to={`${firstEmptySlotId <= 18 ? resourcesPath : villagePath}/${firstEmptySlotId}`}
-        >
-          {buildingName}
-        </Link>
-      </Text>
-    );
-  }
-
   return (
     <Text
       as="span"
+      className="font-semibold"
       data-tooltip-id="general-tooltip"
       data-tooltip-content={t(
-        "Building does not exist and there's no available building spots",
+        '{{buildingName}} does not yet exist in this village',
+        { buildingName },
       )}
     >
       {buildingName}
