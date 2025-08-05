44e8ff7b08f9575b31b69b622762fc456e4fceb7
Jure Rotar <jrotar0@gmail.com>
2025-08-05 15:39:38 +0200

    chore: bumped TypeScript to version 5.9.2, removed unneeded dev build images, updated GH workflow to run on pull requests

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import { defineConfig as defineVitestConfig } from 'vitest/config';
 import { type ManifestOptions, VitePWA } from 'vite-plugin-pwa';
 import { resolve } from 'node:path';
 import { reactRouter } from '@react-router/dev/vite';
-import clsx from 'clsx';
 import tailwindcss from '@tailwindcss/vite';
 import packageJson from './package.json' with { type: 'json' };
 // import { visualizer } from "rollup-plugin-visualizer";
@@ -14,15 +13,11 @@ const graphicsVersion =
   packageJson.dependencies['@pillage-first/graphics'] ?? '0.0.0';
 
 const isInTestMode = process.env.VITEST === 'true';
-// We're setting special icons on non-master environments to differentiate PWAs
 const isDeployingToMaster = process.env.BRANCH_ENV === 'master';
 
-const appNamePostfix = clsx(!isDeployingToMaster && ' - dev');
-const appIconPostfix = clsx(!isDeployingToMaster && '-dev');
-
 const manifest: Partial<ManifestOptions> = {
-  name: `Pillage First! (Ask Questions Later)${appNamePostfix}`,
-  short_name: `Pillage First!${appNamePostfix}`,
+  name: 'Pillage First! (Ask Questions Later)',
+  short_name: 'Pillage First!',
   description:
     'Pillage First! (Ask Questions Later) is a single-player, real-time, browser-based strategy game inspired by Travian. Manage resources to construct buildings, train units, and wage war against your enemies. Remember: pillage first, ask questions later!',
   start_url: '/',
@@ -32,18 +27,18 @@ const manifest: Partial<ManifestOptions> = {
   orientation: 'portrait',
   icons: [
     {
-      src: `/logo${appIconPostfix}-192.png`,
+      src: '/logo-192.png',
       type: 'image/png',
       sizes: '192x192',
     },
     {
-      src: `/logo${appIconPostfix}-512.png`,
+      src: '/logo-512.png',
       type: 'image/png',
       sizes: '512x512',
       purpose: 'maskable',
     },
     {
-      src: `/logo${appIconPostfix}-512.png`,
+      src: '/logo-512.png',
       type: 'image/png',
       sizes: '512x512',
     },
