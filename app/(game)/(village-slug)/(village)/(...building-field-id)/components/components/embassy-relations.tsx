8e0427ef5263b4f5f91cc87989b3ffec78309797
Jure Rotar <jrotar0@gmail.com>
2025-09-02 19:57:44 +0200

    feat: added embassy and town hall building definitions

---
+++ app/(game)/(village-slug)/(village)/(...building-field-id)/components/components/embassy-relations.tsx
@@ -0,0 +1,32 @@
+import { useTranslation } from 'react-i18next';
+import { Alert } from 'app/components/ui/alert';
+import {
+  Section,
+  SectionContent,
+} from 'app/(game)/(village-slug)/components/building-layout';
+import { Bookmark } from 'app/(game)/(village-slug)/(village)/(...building-field-id)/components/components/bookmark';
+import { Text } from 'app/components/text';
+
+export const EmbassyRelations = () => {
+  const { t } = useTranslation();
+
+  return (
+    <Section>
+      <SectionContent>
+        <Bookmark tab="relations" />
+        <Text as="h2">{t('Relations')}</Text>
+        <Text>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium
+          ad autem distinctio nesciunt officia quas qui similique. Aperiam atque
+          et excepturi fugiat labore quidem sed sit tempore totam voluptas.
+          Iure!
+        </Text>
+      </SectionContent>
+      <SectionContent>
+        <Alert variant="warning">
+          {t('This page is still under development')}
+        </Alert>
+      </SectionContent>
+    </Section>
+  );
+};
