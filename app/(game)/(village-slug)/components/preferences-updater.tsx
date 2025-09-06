1db4063fd16eaf0ba58868cbfec92486c6632af2
Jure Rotar <jrotar0@gmail.com>
2025-09-06 12:46:54 +0200

    perf: removed render-causing hooks from top-level layouts, manually memoized top-level layouts (#93)

---
+++ app/(game)/(village-slug)/components/preferences-updater.tsx
@@ -0,0 +1,48 @@
+import clsx from 'clsx';
+import { useEffect } from 'react';
+import { usePreferences } from 'app/(game)/(village-slug)/hooks/use-preferences';
+import { useTextDirection } from 'app/hooks/use-text-direction';
+import layoutStyles from '../layout.module.scss';
+
+export const PreferencesUpdater = () => {
+  const { preferences } = usePreferences();
+
+  const { timeOfDay, skinVariant, colorScheme, locale } = preferences;
+
+  const { direction } = useTextDirection(locale);
+
+  useEffect(() => {
+    const body = document.querySelector('body')!;
+
+    body.classList.add(layoutStyles['background-image']);
+
+    return () => {
+      body.classList.remove(clsx(layoutStyles['background-image']));
+    };
+  }, []);
+
+  useEffect(() => {
+    if (!(colorScheme && skinVariant && timeOfDay)) {
+      return;
+    }
+    const html = document.documentElement;
+
+    html.setAttribute('dir', direction);
+    html.classList.add(
+      colorScheme,
+      `skin-variant-${skinVariant}`,
+      `time-of-day-${timeOfDay}`,
+    );
+
+    return () => {
+      html.removeAttribute('dir');
+      html.classList.remove(
+        colorScheme,
+        `skin-variant-${skinVariant}`,
+        `time-of-day-${timeOfDay}`,
+      );
+    };
+  }, [skinVariant, timeOfDay, colorScheme, direction]);
+
+  return null;
+};
