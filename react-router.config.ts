0123a4775134ba4ec5a670349bbdfbd7d1558436
Jure Rotar <jrotar0@gmail.com>
2025-09-17 17:05:15 +0200

    feat: added script to replace svg placeholders

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,22 +1,8 @@
 import type { Config } from '@react-router/dev/config';
-import { load } from 'cheerio';
-import { mkdir, readFile, writeFile } from 'node:fs/promises';
-import { dirname, join, resolve } from 'node:path';
-
-const PAGES_TO_INCLUDE_PRELOADS_ON = [
-  '/game/server-slug/village-slug/resources',
-  '/game/server-slug/village-slug/resources/building-field-id',
-  '/game/server-slug/village-slug/village',
-  '/game/server-slug/village-slug/village/building-field-id',
-  '/game/server-slug/village-slug/map',
-  '/game/server-slug/village-slug/hero',
-  '/game/server-slug/village-slug/preferences',
-  '/game/server-slug/village-slug/statistics',
-  '/game/server-slug/village-slug/overview',
-  '/game/server-slug/village-slug/quests',
-  '/game/server-slug/village-slug/reports',
-  '/game/server-slug/village-slug/reports/report-id',
-];
+import {
+  createSPAPagesWithPreloads,
+  replaceReactIconsSpritePlaceholdersOnPreRenderedPages,
+} from './scripts/react-router-build-end';
 
 export default {
   ssr: false,
@@ -35,62 +21,8 @@ export default {
     unstable_viteEnvironmentApi: true,
     unstable_splitRouteModules: 'enforce',
   },
-  buildEnd: async () => {
-    const clientDir = resolve('build/client');
-
-    const fallbackPath = join(clientDir, 'index.html');
-    const prefetchHtmlPath = join(clientDir, '__spa-preload', 'index.html');
-
-    const [fallbackHtml, prefetchHtml] = await Promise.all([
-      readFile(fallbackPath, 'utf8'),
-      readFile(prefetchHtmlPath, 'utf8'),
-    ]);
-
-    const $prefetch = load(prefetchHtml);
-    const importMapScript = $prefetch('script[type="importmap"]').html();
-    const integrityMap = importMapScript
-      ? (JSON.parse(importMapScript).integrity ?? {})
-      : {};
-
-    for (const page of PAGES_TO_INCLUDE_PRELOADS_ON) {
-      const $fallback = load(fallbackHtml);
-
-      const matchingLinks = $prefetch(
-        `link[data-prefetch-page="${page}"]`,
-      ).toArray();
-
-      matchingLinks.forEach((el) => {
-        const $link = $prefetch(el);
-        const href = $link.attr('href');
-
-        if (!href) {
-          return;
-        }
-
-        // Skip if a link with the same href already exists in the fallback
-        const alreadyExists = $fallback(`link[href="${href}"]`).length > 0;
-        if (alreadyExists) {
-          return;
-        }
-
-        $link.removeAttr('data-prefetch-page');
-
-        if (href.endsWith('.js')) {
-          $link.attr('as', 'script');
-          $link.attr('crossorigin', 'anonymous');
-        }
-
-        const integrity = integrityMap[href];
-        if (integrity) {
-          $link.attr('integrity', integrity);
-        }
-
-        $fallback('head').append($prefetch.html(el));
-      });
-
-      const outputPath = join(clientDir, page.replace(/^\//, ''), 'index.html');
-      await mkdir(dirname(outputPath), { recursive: true });
-      await writeFile(outputPath, $fallback.html(), 'utf8');
-    }
+  buildEnd: async (args) => {
+    await createSPAPagesWithPreloads(args);
+    await replaceReactIconsSpritePlaceholdersOnPreRenderedPages(args);
   },
 } satisfies Config;
---
+++ scripts/react-router-build-end.ts
@@ -0,0 +1,98 @@
+import type { Config } from '@react-router/dev/config';
+import { dirname, join, resolve } from 'node:path';
+import { mkdir, readFile, writeFile } from 'node:fs/promises';
+import { load } from 'cheerio';
+import { glob } from 'tinyglobby';
+
+export const createSPAPagesWithPreloads: NonNullable<
+  Config['buildEnd']
+> = async () => {
+  const PAGES_TO_INCLUDE_PRELOADS_ON = [
+    '/game/server-slug/village-slug/resources',
+    '/game/server-slug/village-slug/resources/building-field-id',
+    '/game/server-slug/village-slug/village',
+    '/game/server-slug/village-slug/village/building-field-id',
+    '/game/server-slug/village-slug/map',
+    '/game/server-slug/village-slug/hero',
+    '/game/server-slug/village-slug/preferences',
+    '/game/server-slug/village-slug/statistics',
+    '/game/server-slug/village-slug/overview',
+    '/game/server-slug/village-slug/quests',
+    '/game/server-slug/village-slug/reports',
+    '/game/server-slug/village-slug/reports/report-id',
+  ];
+
+  const clientDir = resolve('build/client');
+
+  const fallbackPath = join(clientDir, 'index.html');
+  const prefetchHtmlPath = join(clientDir, '__spa-preload', 'index.html');
+
+  const [fallbackHtml, prefetchHtml] = await Promise.all([
+    readFile(fallbackPath, 'utf8'),
+    readFile(prefetchHtmlPath, 'utf8'),
+  ]);
+
+  const $prefetch = load(prefetchHtml);
+
+  for (const page of PAGES_TO_INCLUDE_PRELOADS_ON) {
+    const $fallback = load(fallbackHtml);
+
+    const matchingLinks = $prefetch(
+      `link[data-prefetch-page="${page}"]`,
+    ).toArray();
+
+    matchingLinks.forEach((el) => {
+      const $link = $prefetch(el);
+      const href = $link.attr('href');
+
+      if (!href) {
+        return;
+      }
+
+      // Skip if a link with the same href already exists in the fallback
+      const alreadyExists = $fallback(`link[href="${href}"]`).length > 0;
+      if (alreadyExists) {
+        return;
+      }
+
+      $link.removeAttr('data-prefetch-page');
+
+      if (href.endsWith('.js')) {
+        $link.attr('as', 'script');
+        $link.attr('crossorigin', 'anonymous');
+      }
+
+      $fallback('head').append($prefetch.html(el));
+    });
+
+    const outputPath = join(clientDir, page.replace(/^\//, ''), 'index.html');
+    await mkdir(dirname(outputPath), { recursive: true });
+    await writeFile(outputPath, $fallback.html());
+  }
+};
+
+export const replaceReactIconsSpritePlaceholdersOnPreRenderedPages: NonNullable<
+  Config['buildEnd']
+> = async ({ reactRouterConfig }) => {
+  const clientDir = resolve('build/client');
+  const [svgSpriteFile] = await glob(
+    './build/client/assets/react-icons-sprite-*.svg',
+  );
+  const svgSpriteName = svgSpriteFile.replace('build/client', '');
+
+  const preRenderedFileUrls = (reactRouterConfig.prerender as string[]).map(
+    (path) => {
+      return resolve(clientDir, `.${path}`, 'index.html');
+    },
+  );
+
+  for (const filePath of preRenderedFileUrls) {
+    const content = await readFile(filePath, 'utf-8');
+    const updatedContent = content.replaceAll(
+      '__SPRITE_URL_PLACEHOLDER__',
+      svgSpriteName,
+    );
+
+    await writeFile(filePath, updatedContent, 'utf-8');
+  }
+};
