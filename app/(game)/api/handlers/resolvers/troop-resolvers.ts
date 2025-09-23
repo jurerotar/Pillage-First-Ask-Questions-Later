03cb857af81f0a3765873ebbb6edbd6389ed76de
Jure Rotar <jrotar0@gmail.com>
2025-09-23 11:06:12 +0200

    chore: removed manual tileId in unit training resolvers

--- app/(game)/api/handlers/resolvers/troop-resolvers.ts
+++ app/(game)/api/handlers/resolvers/troop-resolvers.ts
@@ -4,17 +4,22 @@ import type { Troop } from 'app/interfaces/models/game/troop';
 import {
   effectsCacheKey,
   troopsCacheKey,
+  villagesCacheKey,
 } from 'app/(game)/(village-slug)/constants/query-keys';
 import { getUnitData } from 'app/(game)/(village-slug)/utils/units';
 import { modifyTroops } from 'app/(game)/api/handlers/resolvers/utils/troops';
 import type { Effect } from 'app/interfaces/models/game/effect';
 import { isVillageEffect } from 'app/(game)/(village-slug)/hooks/guards/effect-guards';
 import { updateVillageResourcesAt } from 'app/(game)/api/utils/village';
+import type { Village } from 'app/interfaces/models/game/village';
 
 export const troopTrainingEventResolver: Resolver<
   GameEvent<'troopTraining'>
 > = async (queryClient, args) => {
-  const { unitId, villageId, tileId, duration, startsAt } = args;
+  const { unitId, villageId, duration, startsAt } = args;
+
+  const villages = queryClient.getQueryData<Village[]>([villagesCacheKey])!;
+  const { tileId } = villages.find(({ id }) => id === villageId)!;
 
   const { unitWheatConsumption } = getUnitData(unitId);
 
