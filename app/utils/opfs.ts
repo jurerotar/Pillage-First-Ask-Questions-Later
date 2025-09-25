302545f5af6dab0bffc1f0853ccd8a6250caa007
Jure Rotar <jrotar0@gmail.com>
2025-09-25 08:20:46 +0200

    perf: excluded happy-dom types from being compiled
    - this change results in ~10% increased compilation speed

--- app/utils/opfs.ts
+++ app/utils/opfs.ts
@@ -1,3 +1,5 @@
+/// <reference lib="webworker" />
+
 export const getRootHandle = async (): Promise<FileSystemDirectoryHandle> => {
   const root = await navigator.storage.getDirectory();
   return root.getDirectoryHandle('pillage-first-ask-questions-later', {
