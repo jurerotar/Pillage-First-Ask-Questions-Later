f626861e0ef72bc751e4b37fd6625fd2a8675334
Jure Rotar <jrotar0@gmail.com>
2025-11-10 17:31:21 +0100

    chore: expanded create-new-server test suite

--- app/db/seeders/guaranteed-croppers-seeder.ts
+++ app/db/seeders/guaranteed-croppers-seeder.ts
@@ -27,11 +27,13 @@ export const guaranteedCroppersSeeder: Seeder = (database, server) => {
           WHERE
             o.resource = 'wheat'
             AND o.bonus = 50
+            AND ot.type = 'oasis'
           GROUP BY ot.id, ot.x, ot.y
           ) AS ot
           JOIN tiles t
                ON t.x BETWEEN ot.x - 3 AND ot.x + 3
                  AND t.y BETWEEN ot.y - 3 AND ot.y + 3
+                 AND t.type = 'free'
       ;
     `,
   );
