c627e168206ca123f8e91557f31295b83050b379
Jure Rotar <jrotar0@gmail.com>
2025-11-25 08:19:29 +0100

    fix: building upgrade events are now immediately completed upon enabling developer mode

--- app/(game)/api/handlers/preferences-handlers.ts
+++ app/(game)/api/handlers/preferences-handlers.ts
@@ -1,6 +1,12 @@
 import type { ApiHandler } from 'app/interfaces/api';
 import type { Preferences } from 'app/interfaces/models/game/preferences';
-import { preferencesCacheKey } from 'app/(game)/(village-slug)/constants/query-keys';
+import {
+  eventsCacheKey,
+  preferencesCacheKey,
+} from 'app/(game)/(village-slug)/constants/query-keys';
+import type { GameEvent } from 'app/interfaces/models/game/game-event';
+import { isBuildingLevelUpEvent } from 'app/(game)/guards/event-guards';
+import { scheduleNextEvent } from 'app/(game)/api/utils/event-resolvers';
 
 export const getPreferences: ApiHandler<Preferences> = async (queryClient) => {
   return queryClient.getQueryData<Preferences>([preferencesCacheKey])!;
@@ -29,4 +35,19 @@ export const updatePreference: ApiHandler<
       };
     },
   );
+
+  if (preferenceName === 'isDeveloperModeEnabled' && value === true) {
+    queryClient.setQueryData<GameEvent[]>([eventsCacheKey], (events) => {
+      for (const event of events!) {
+        if (isBuildingLevelUpEvent(event)) {
+          event.startsAt = Date.now();
+          event.duration = 0;
+        }
+      }
+
+      scheduleNextEvent(queryClient);
+
+      return events;
+    });
+  }
 };
