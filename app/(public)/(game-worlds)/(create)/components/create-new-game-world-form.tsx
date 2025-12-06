9db79c2cc1c84d766cfdaa0fad3e59efb4b12b2b
bfreeman915 <bfreeman915@gmail.com>
2025-12-06 06:00:57 -0600

    feat: added hospital information (#105)
    Co-authored-by: Jure Rotar <jrotar0@gmail.com>

--- app/(public)/(game-worlds)/(create)/components/create-new-game-world-form.tsx
+++ app/(public)/(game-worlds)/(create)/components/create-new-game-world-form.tsx
@@ -1,6 +1,6 @@
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
-import { z } from 'zod/v4';
+import { z } from 'zod';
 import {
   Form,
   FormControl,
