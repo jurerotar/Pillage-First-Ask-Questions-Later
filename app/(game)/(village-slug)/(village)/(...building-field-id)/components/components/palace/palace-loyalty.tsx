ce925a570fe0d94a184687d48dcd2a178dadc221
Freeman <bfreeman915@gmail.com>
2025-12-07 14:13:14 -0600

    feat: added residence loyalty info and fixed relations tab name in embassy

--- app/(game)/(village-slug)/(village)/(...building-field-id)/components/components/palace/palace-loyalty.tsx
+++ app/(game)/(village-slug)/(village)/(...building-field-id)/components/components/palace/palace-loyalty.tsx
@@ -16,11 +16,40 @@ export const PalaceLoyalty = () => {
         <Bookmark tab="loyalty" />
         <Text as="h2">{t('Loyalty')}</Text>
         <Text>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium
-          ad autem distinctio nesciunt officia quas qui similique. Aperiam atque
-          et excepturi fugiat labore quidem sed sit tempore totam voluptas.
-          Iure!
+          {t(
+            "Every village begins with 100% loyalty. Loyalty determines how firmly a village belongs to your realm. When loyalty drops to 0%, the village will switch allegiance and become part of the attacker's realm.",
+          )}
         </Text>
+        <Text as="h3">{t('How Loyalty Works')}</Text>
+        <Text>
+          {t(
+            "A village's loyalty cannot decrease as long as it has one of the following buildings:",
+          )}
+        </Text>
+        <ul className="list-disc pl-4">
+          <li>{t('Residence')}</li>
+          <li>{t('Palace')}</li>
+        </ul>
+        <Text>
+          {t(
+            'If none of these buildings are present, loyalty can be reduced by attacks with administrators.',
+          )}
+        </Text>
+        <Text>{t('Once loyalty reaches 0%, the village is conquered.')}</Text>
+        <Text as="h3">{t('Increasing Loyalty')}</Text>
+        <Text>
+          {t(
+            'Loyalty automatically increases over time when a Residence or Palace exists in the village.',
+          )}
+        </Text>
+        <ul className="list-disc pl-4">
+          <li>
+            {t(
+              'Each builing level increases loyalty by 2 points every three hours, up to a maximum of 100%.',
+            )}
+          </li>
+          <li>{t('Certain hero items may further increase loyalty.')}</li>
+        </ul>
       </SectionContent>
       <SectionContent>
         <Alert variant="warning">
