175491b91a0efb6de0abb405b802a85a2a1c05f7
Jure Rotar <jrotar0@gmail.com>
2025-12-24 23:26:32 +0100

    chore: format

--- app/(game)/(village-slug)/(players)/middleware/redirect-to-statistics-middleware.ts
+++ app/(game)/(village-slug)/(players)/middleware/redirect-to-statistics-middleware.ts
@@ -1,10 +1,9 @@
 import type { Route } from '.react-router/types/app/(game)/(village-slug)/(players)/+types/page';
 import { redirect } from 'react-router';
 
-export const redirectToStatisticsMiddleware: Route.ClientMiddlewareFunction = async ({
-  params,
-}) => {
-  const { serverSlug } = params;
+export const redirectToStatisticsMiddleware: Route.ClientMiddlewareFunction =
+  async ({ params }) => {
+    const { serverSlug } = params;
 
-  throw redirect(`/game/${serverSlug}/v-1/statistics`);
-};
+    throw redirect(`/game/${serverSlug}/v-1/statistics`);
+  };
