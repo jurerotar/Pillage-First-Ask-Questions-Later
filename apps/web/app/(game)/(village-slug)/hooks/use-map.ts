a39d987903c2b97f33c5d4039d6b81e860e7ab05
mkzxc <mkzxc@gmail.com>
2026-01-11 18:38:26 +0100

    fix: missing border tiles linked Tile object

--- apps/web/app/(game)/(village-slug)/hooks/use-map.ts
+++ apps/web/app/(game)/(village-slug)/hooks/use-map.ts
@@ -1,14 +1,46 @@
 import { useSuspenseQuery } from '@tanstack/react-query';
-import { use } from 'react';
+import { use, useCallback } from 'react';
 import { z } from 'zod';
 import { tileSchema } from '@pillage-first/types/models/tile';
+import {
+  encodeGraphicsProperty,
+  tileIdToCoordinates,
+} from '@pillage-first/utils/map';
 import { ApiContext } from 'app/(game)/providers/api-provider';
+import { useServer } from './use-server';
 
 // We don't save border tiles to db, so they come back as null;
-const mapSchema = tileSchema.nullable();
+const tilesApiSchema = z.array(tileSchema.nullable());
+const mapSchema = z.array(tileSchema);
+
+export const BORDER_TILES_OASIS_VARIANTS = [1, 2, 3, 4];
+const BORDER_TILES_OASIS_GRAPHICS = BORDER_TILES_OASIS_VARIANTS.map((variant) =>
+  encodeGraphicsProperty('wood', 0, 0, 0, variant),
+);
 
 export const useMap = () => {
   const { fetcher } = use(ApiContext);
+  const { mapSize } = useServer();
+
+  const mapNullTilesToOasisTiles = useCallback(
+    (data: z.infer<typeof tilesApiSchema>): z.infer<typeof mapSchema> => {
+      return data.map(
+        (item, idx) =>
+          item || {
+            type: 'oasis',
+            id: idx + 1,
+            coordinates: tileIdToCoordinates(idx + 1, mapSize),
+            owner: null,
+            ownerVillage: null,
+            attributes: {
+              oasisGraphics: BORDER_TILES_OASIS_GRAPHICS[idx % 4],
+              isOccupiable: false,
+            },
+          },
+      );
+    },
+    [mapSize],
+  );
 
   const { data: map } = useSuspenseQuery({
     queryKey: ['tiles'],
@@ -17,8 +49,9 @@ export const useMap = () => {
       // What we should do is remove all the non-static parts (world items, troop movements,...) so that this query can be permanently cached.
       const { data } = await fetcher('/tiles');
 
-      return z.array(mapSchema).parse(data);
+      return tilesApiSchema.parse(data);
     },
+    select: mapNullTilesToOasisTiles,
   });
 
   return {
