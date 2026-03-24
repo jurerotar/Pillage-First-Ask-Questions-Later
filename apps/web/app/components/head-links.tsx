dc7141ec1005da1cda7fd1274c1268924df59ceb
Jure Rotar <jrotar0@gmail.com>
2026-03-24 11:42:31 +0100

    feat: updated social media preview tags (#175)

--- apps/web/app/components/head-links.tsx
+++ apps/web/app/components/head-links.tsx
@@ -1,7 +1,10 @@
+import { useTranslation } from 'react-i18next';
 import { env } from '@pillage-first/utils/env';
 import { SqlitePrefetchLink } from 'app/components/sqlite-prefetch-link.tsx';
 
 export const HeadLinks = () => {
+  const { i18n } = useTranslation();
+
   return (
     <>
       {env.MODE === 'production' && (
@@ -17,20 +20,88 @@ export const HeadLinks = () => {
           />
         </>
       )}
+      <meta
+        name="description"
+        content="Pillage First! (Ask Questions Later) is an open-source, single-player, strategy game inspired by Travian. Build villages, manage resources, train troops, start adventures and wage war in persistent, offline-first game worlds."
+      />
+      <meta
+        name="twitter:card"
+        content="summary_large_image"
+      />
+      <meta
+        name="twitter:site"
+        content="@pillagefirst"
+      />
+      <meta
+        name="twitter:creator"
+        content="@pillagefirst"
+      />
+      <meta
+        name="twitter:title"
+        content="Pillage First! (Ask Questions Later)"
+      />
+      <meta
+        name="twitter:description"
+        content="Pillage First! (Ask Questions Later) is an open-source, single-player, strategy game inspired by Travian. Build villages, manage resources, train troops, start adventures and wage war in persistent, offline-first game worlds."
+      />
+      <meta
+        name="twitter:image"
+        content={`${env.DEPLOY_PRIME_URL}/pillage-first-logo.png?v=${env.GRAPHICS_VERSION}`}
+      />
+      <meta
+        name="twitter:url"
+        content={env.DEPLOY_PRIME_URL}
+      />
+      <meta
+        property="og:url"
+        content={env.DEPLOY_PRIME_URL}
+      />
+      <meta
+        property="og:type"
+        content="website"
+      />
+      <meta
+        property="og:title"
+        content="Pillage First! (Ask Questions Later)"
+      />
+      <meta
+        property="og:description"
+        content="Pillage First! (Ask Questions Later) is an open-source, single-player, strategy game inspired by Travian. Build villages, manage resources, train troops, start adventures and wage war in persistent, offline-first game worlds."
+      />
+      <meta
+        property="og:image"
+        content={`${env.DEPLOY_PRIME_URL}/pillage-first-logo.png?v=${env.GRAPHICS_VERSION}`}
+      />
+      <meta
+        property="og:image:width"
+        content="920"
+      />
+      <meta
+        property="og:image:height"
+        content="919"
+      />
+      <meta
+        property="og:locale"
+        content={i18n.language.replace('-', '_')}
+      />
+      <meta
+        property="og:site_name"
+        content="Pillage First!"
+      />
       <link
         rel="icon"
         type="image/png"
-        href={`/favicon-96x96.png?v=${env.GRAPHICS_VERSION}`}
+        href={`/favicon/favicon-96x96.png?v=${env.GRAPHICS_VERSION}`}
         sizes="96x96"
       />
       <link
         rel="shortcut icon"
-        href={`/favicon.ico?v=${env.GRAPHICS_VERSION}`}
+        href={`/favicon/favicon.ico?v=${env.GRAPHICS_VERSION}`}
       />
       <link
         rel="apple-touch-icon"
         sizes="180x180"
-        href={`/apple-touch-icon.png?v=${env.GRAPHICS_VERSION}`}
+        href={`/favicon/apple-touch-icon.png?v=${env.GRAPHICS_VERSION}`}
       />
       <meta
         name="viewport"
--- packages/utils/src/env.ts
+++ packages/utils/src/env.ts
@@ -5,6 +5,9 @@ type ImportMetaEnv = {
   readonly GRAPHICS_VERSION: string;
   readonly VITE_FARO_INGEST_ENDPOINT: string;
   // Injected by Netlify, not available during dev
+  readonly URL: string;
+  readonly DEPLOY_URL: string;
+  readonly DEPLOY_PRIME_URL: string;
   readonly COMMIT_REF: string;
   readonly HEAD: string;
 };
@@ -26,6 +29,9 @@ export const env = {
   VERSION: import.meta.env.VERSION,
   GRAPHICS_VERSION: import.meta.env.GRAPHICS_VERSION,
   VITE_FARO_INGEST_ENDPOINT: import.meta.env.VITE_FARO_INGEST_ENDPOINT,
+  URL: import.meta.env.URL,
+  DEPLOY_URL: import.meta.env.DEPLOY_URL,
+  DEPLOY_PRIME_URL: import.meta.env.DEPLOY_PRIME_URL,
   COMMIT_REF: import.meta.env.COMMIT_REF,
   HEAD: import.meta.env.HEAD,
 };
