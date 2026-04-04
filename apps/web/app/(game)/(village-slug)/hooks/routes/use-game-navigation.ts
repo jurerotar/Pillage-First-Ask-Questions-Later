7bfa77834f6f43b43857dfa1c5cee3125d59eae5
Jure Rotar <jrotar0@gmail.com>
2026-04-04 12:48:45 +0200

    fix: fixed enter village link text and styling

--- apps/web/app/(game)/(village-slug)/hooks/routes/use-game-navigation.ts
+++ apps/web/app/(game)/(village-slug)/hooks/routes/use-game-navigation.ts
@@ -1,10 +1,19 @@
 import { useCallback } from 'react';
 import { useLocation } from 'react-router';
 import { useCurrentVillage } from 'app/(game)/(village-slug)/hooks/current-village/use-current-village.ts';
+import { useServer } from 'app/(game)/(village-slug)/hooks/use-server.ts';
 
 export const useGameNavigation = () => {
   const { currentVillage } = useCurrentVillage();
   const { pathname, search } = useLocation();
+  const { serverSlug } = useServer();
+
+  const getVillageBasePath = useCallback(
+    (slug: string) => {
+      return `/game/${serverSlug}/${slug}`;
+    },
+    [serverSlug],
+  );
 
   const getNewVillageUrl = useCallback(
     (slug: string) => {
@@ -15,5 +24,6 @@ export const useGameNavigation = () => {
 
   return {
     getNewVillageUrl,
+    getVillageBasePath,
   };
 };
