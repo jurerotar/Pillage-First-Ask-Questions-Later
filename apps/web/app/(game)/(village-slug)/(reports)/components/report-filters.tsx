5badfdc634f15c9c0baa75a89b9d94520dc12894
AllNineAliens <AllNineAliens@protonmail.com>
2026-07-03 21:45:57 +0200

    fix: add missing file

---
+++ apps/web/app/(game)/(village-slug)/(reports)/components/report-filters.tsx
@@ -0,0 +1,54 @@
+import { useTranslation } from 'react-i18next';
+import { LuScale, LuSword } from 'react-icons/lu';
+import { PiPathBold } from 'react-icons/pi';
+import type { ReportType } from '@pillage-first/types/models/report';
+import { SectionContent } from 'app/(game)/(village-slug)/components/building-layout';
+import { Text } from 'app/components/text';
+import { ToggleGroup, ToggleGroupItem } from 'app/components/ui/toggle-group';
+
+type ReportFiltersProps = {
+  reportFilters: ReportType[];
+  onChange: (reportFilters: ReportType[]) => void;
+};
+
+export const ReportFilters = ({
+  reportFilters,
+  onChange,
+}: ReportFiltersProps) => {
+  const { t } = useTranslation();
+
+  return (
+    <SectionContent>
+      <Text className="font-semibold">{t('Filter reports')}</Text>
+      <ToggleGroup
+        type="multiple"
+        value={reportFilters}
+        onValueChange={onChange}
+        variant="outline"
+        size="sm"
+      >
+        <ToggleGroupItem
+          data-tooltip-id="general-tooltip"
+          data-tooltip-content={t('Toggle battle reports')}
+          value="battle"
+        >
+          <LuSword className="size-4" />
+        </ToggleGroupItem>
+        <ToggleGroupItem
+          data-tooltip-id="general-tooltip"
+          data-tooltip-content={t('Toggle adventure reports')}
+          value="adventure"
+        >
+          <PiPathBold className="size-4" />
+        </ToggleGroupItem>
+        <ToggleGroupItem
+          data-tooltip-id="general-tooltip"
+          data-tooltip-content={t('Toggle trade reports')}
+          value="trade"
+        >
+          <LuScale className="size-4" />
+        </ToggleGroupItem>
+      </ToggleGroup>
+    </SectionContent>
+  );
+};
