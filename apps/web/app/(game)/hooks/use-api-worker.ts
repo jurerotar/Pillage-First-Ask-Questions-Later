1021da5063d19ae8b1b6fdd27a58aa72ae49798d
Jure Rotar <jrotar0@gmail.com>
2026-08-19 00:02:24 +0200

    feat: game world splash screen

--- apps/web/app/(game)/hooks/use-api-worker.ts
+++ apps/web/app/(game)/hooks/use-api-worker.ts
@@ -2,10 +2,25 @@ import { useSuspenseQuery } from '@tanstack/react-query';
 import type { Server } from '@pillage-first/types/models/server';
 import { getApiWorkerHandle } from 'app/(game)/providers/utils/api-worker-manager';
 
+const MIN_API_PROVIDER_SPLASH_DURATION_MS = 1_000;
+
+const wait = (duration: number): Promise<void> => {
+  return new Promise((resolve) => {
+    globalThis.setTimeout(resolve, duration);
+  });
+};
+
 export const useApiWorker = (serverSlug: Server['slug']) => {
   const { data: apiWorkerHandle } = useSuspenseQuery({
     queryKey: ['api-worker', serverSlug],
-    queryFn: async () => getApiWorkerHandle(serverSlug),
+    queryFn: async () => {
+      const [apiWorkerHandle] = await Promise.all([
+        getApiWorkerHandle(serverSlug),
+        wait(MIN_API_PROVIDER_SPLASH_DURATION_MS),
+      ]);
+
+      return apiWorkerHandle;
+    },
     staleTime: Number.POSITIVE_INFINITY,
     gcTime: Number.POSITIVE_INFINITY,
   });
