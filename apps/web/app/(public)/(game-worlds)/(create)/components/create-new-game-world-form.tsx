dc1a0f8b2600ae18566027ab60e65e51af9feb5c
Jure Rotar <jrotar0@gmail.com>
2026-09-10 22:35:07 +0200

    chore: updated error message

--- apps/web/app/(public)/(game-worlds)/(create)/components/create-new-game-world-form.tsx
+++ apps/web/app/(public)/(game-worlds)/(create)/components/create-new-game-world-form.tsx
@@ -522,13 +522,10 @@ export const CreateNewGameWorldForm = () => {
                 Browser storage is unavailable
               </Text>
               <Alert variant="error">
-                Pillage First! requires browser storage to function properly.
-                Your browser does not allow data storage. Game worlds cannot be
-                created in this browser mode.'
-                <br />
-                <br />
-                In Safari, turn off Private Browsing or allow website storage,
-                then try again.
+                Pillage First! requires access to browser storage to function
+                properly. Your browser does not allow data storage. This issue
+                happens most often in iOS Safari's Private Mode. Please turn off
+                Private Mode, then try again.
               </Alert>
             </div>
             <div className="flex justify-end">
